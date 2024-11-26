183d55d8a376e3ce3dc20a417f26a6273529f143
jbarney14 <157840954+jbarney14@users.noreply.github.com>
2024-11-25 20:49:46 -0500

    barney teleporter grate

---
+++ Beyond the Lock/Assets/teleporterGrate/pressureUp.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressureUp : MonoBehaviour
+{
+
+    public GameObject spikes;
+    bool isPressed = false;
+   void OnTriggerEnter(Collider col) {
+    if(!isPressed) {
+    spikes.transform.position += new Vector3(0, 5, 0);
+     isPressed = true;
+    }
+    
+   }
+}
