4b9bd2a53422c235a319ca5389c3b1533f6c14e3
jbarney14 <157840954+jbarney14@users.noreply.github.com>
2024-12-01 01:43:09 -0500

    added laser room, cam room, vault background noise

--- Beyond the Lock/Assets/teleporterGrate/pressureUp.cs
+++ Beyond the Lock/Assets/teleporterGrate/pressureUp.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class pressureUp : MonoBehaviour
 {
+    public GameObject spikes; // The spikes object to raise
+    public float damageAmount = 10f; // Damage dealt by the spikes
+    private bool isPressed = false;
 
-    public GameObject spikes;
-    bool isPressed = false;
-   void OnTriggerEnter(Collider col) {
-    if(!isPressed) {
-    spikes.transform.position += new Vector3(0, .75f, 0);
-     isPressed = true;
+    void OnTriggerEnter(Collider col)
+    {
+        if (!isPressed)
+        {
+            // Raise the spikes
+            spikes.transform.position += new Vector3(0, .75f, 0);
+            isPressed = true;
+
+            // Check if the colliding object has the "Player" tag
+            if (col.CompareTag("Player")) // Make sure the player is tagged as "Player"
+            {
+                // Access the PlayerHealth component attached to the Player
+                PlayerHealth playerHealth = col.GetComponent<PlayerHealth>();
+                if (playerHealth != null)
+                {
+                    // Call the TakeDamage method
+                    playerHealth.TakeDamage(damageAmount);
+                }
+                else
+                {
+                    Debug.LogWarning("No PlayerHealth script found on the player!");
+                }
+            }
+        }
     }
-    
-   }
 }
